1d9c33ce6658fcac8fbbf64840a1b5a84cf0b138
du yeong <doyeong32@gmail.com>
2023-05-07 21:58:02 +0900

    최종본

--- 수신/Receiver.c
+++ 수신/Receiver.c
@@ -1,7 +1,8 @@
 #include <windows.h>
 #include <stdio.h>
 #include <stdint.h>
-#include "CRC16CCITT.c"
+#include "RECEIVER_CRC16CCITT.c"
+int error_count=0;
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
@@ -42,24 +43,26 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
         case WM_COPYDATA: {
             COPYDATASTRUCT* pcds = (COPYDATASTRUCT*)lParam;
-            // 전송된 데이터가 있는지 확인하고 출력합니다
+            // 전송된 데이터가 있는지 확인하고 처리 결과를 출력합니다.
             if (pcds != NULL && pcds->cbData > 0 && pcds->lpData != NULL) {
                 int index = (int)wParam;
-                const char* data = (const char*)pcds->lpData;
+                const uint8_t* data = (const uint8_t*)pcds->lpData;
                 int datasize = pcds->cbData;
 
-                // 전달받은 문자열 출력
-                // printf("%s\n", data);
-
-                // 문자열을 1바이트 단위로 쪼개어 uint8_t배열로 저장
-                uint8_t codeword_byte[datasize];
-                for(int i=0; i<datasize; i++){
-                    codeword_byte[i]=(uint8_t)data[i];
-                }
+                //전송받은 데이터 출력
+                // printf("index : %d\n", index);
+                // for(int i=0; i<datasize; i++){
+                //     printf("%x ", data[i]);
+                // }
                 
                 // CRC검사 결과 출력
                 // crc_check 계산값이 0이면 OK 출력 0이상이면 ERROR 출력
-                printf("%s\n", crc16_ccitt(data,datasize)==0?"OK":"ERROR");
+                int result = crc16_ccitt(data,datasize);
+                if(result==1){
+                    error_count++;
+                }
+                printf("index : %d %s ",index, result==0?"OK":"ERROR");
+                printf("/ error_count : %d\n", error_count);
             }
 
             // 메모리를 해제합니다
